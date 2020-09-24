b392d8fefcccbef6414fade79ca5a737e78ba2b9
Nicholas dela Fuente <nick.delafuente1@gmail.com>
2020-09-24 10:14:51 -0600

    Deploying to Heroku

--- twitoff/twitter.py
+++ twitoff/twitter.py
@@ -16,7 +16,7 @@ TWITTER = tweepy.API(TWITTER_AUTH)
 
 # loading in nlp model and returning 300 size embedding
 def vectorize_tweet(tweet_text):
-    nlp = spacy.load("en_core_web_md")
+    nlp = spacy.load("en_core_web_sm")
     return nlp(tweet_text).vector
 
 def add_or_update_user(username):
